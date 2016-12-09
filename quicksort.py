7f994571ce214228d856e9312fa48023cbdde313
Nolan Miller <nolanrm20@gmail.com>
2016-12-08 22:25:53 -0600

    Add quicksort algorithm

---
+++ quicksort.py
@@ -0,0 +1,30 @@
+""" Implementation of the quicksort algorithm """
+
+def quicksort(in_list):
+	""" 
+		:parameter in_list: an unsorted and mutable list to be sorted
+
+		will return sorted in_list
+	"""
+	if len(in_list) <= 1:
+		return in_list
+
+	smaller = []
+	larger = []
+	equal = []
+	pivot = in_list[len(in_list) // 2]
+
+	for item in in_list:
+		if item < pivot:
+			smaller.append(item)
+		elif item > pivot:
+			larger.append(item)
+		else:
+			equal.append(item)
+	return quicksort(smaller) + equal + quicksort(larger)
+
+
+test_array = [100, 45, 12, 34, 60, 3, 4, 5, 20]
+print("initial array : ", test_array)
+print("sorted array : ", quicksort(test_array))
+
